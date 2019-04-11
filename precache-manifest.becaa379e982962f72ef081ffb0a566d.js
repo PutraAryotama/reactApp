6a471bc244221a7ff4e941fe149f89dfe42a6821
Putra Aryotama <putraaryotama@gmail.com>
2019-04-11 17:28:20 +0700

    Updates

---
+++ precache-manifest.becaa379e982962f72ef081ffb0a566d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3c342db9e71363a321fa",
+    "url": "/reactApp/static/css/main.34560c43.chunk.css"
+  },
+  {
+    "revision": "3c342db9e71363a321fa",
+    "url": "/reactApp/static/js/main.dde58e32.chunk.js"
+  },
+  {
+    "revision": "4f6bef0ec26ba2365171",
+    "url": "/reactApp/static/js/runtime~main.56b3556b.js"
+  },
+  {
+    "revision": "068e490c215809f4a686",
+    "url": "/reactApp/static/js/2.49216703.chunk.js"
+  },
+  {
+    "revision": "8a23d8d025bd7f142e256db4f924fbc6",
+    "url": "/reactApp/index.html"
+  }
+];
